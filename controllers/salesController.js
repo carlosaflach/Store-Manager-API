b8e2769a717f88e853da0bd96d7b2242d661d2e7
Carlos Afonso <carlos.flach15@gmail.com>
2022-06-30 18:40:02 -0300

    feat: Implements sales service function to create a sale.

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -1,7 +1,8 @@
 const SalesService = require('../services/salesService');
 
-const createSale = async (req, res, _next) => {
+const createSale = async (req, res, next) => {
   const newSale = await SalesService.createSale(req.body);
+  if (newSale.error) return next(newSale.error);
 
   return res.status(201).json(newSale);
 };
--- services/salesService.js
+++ services/salesService.js
@@ -1,7 +1,17 @@
 const SalesModel = require('../models/salesModel');
-// const ProducstModel = require('../models/productsModel');
+const ProducstModel = require('../models/productsModel');
 
 const createSale = async (array) => {
+  const productExists = await Promise.all(array.map((s) => ProducstModel.findById(s.productId)));
+  const check = productExists.some((p) => p === undefined);
+  if (check) {
+    return {
+      error: {
+        code: 'notFound',
+        message: 'Product not found',
+      },
+    };
+  }
   const newSale = await SalesModel.createSale(array);
   return newSale;
 };
